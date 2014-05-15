acd8d0ed87f90089aa73001c2b98630b6593fb58
phanvannhien <phanvannhien@gmail.com>
2014-05-15 10:52:05 +0700

    Update chat-dsv.js
    Update link stream server

--- assets/js/chat-dsv.js
+++ assets/js/chat-dsv.js
@@ -643,7 +643,8 @@
 		$(".nano").nanoScroller({ preventPageScrolling: true });
 		$.contextMenu(chat.oContextMenu);
 		//Run flash Server
-		var flashvars = {sRTMP:"<%- sRTMP %>"};
+		//var flashvars = {sRTMP:"<%- sRTMP %>"};
+		var flashvars = "rtmp://www.talkingheads.tream.co.uk/talkingheads";
 		// Run flash Localhost
 		//var flashvars = {sRTMP:"rtmp://localhost/SOSample"};
 		var params = {};
