6d37ee4191cef26a80234b700f58b4abd681797a
phanvannhien <phanvannhien@gmail.com>
2014-05-30 14:17:57 +0700

    Finish comments code and refactor file Favorites.js

--- api/models/Favorites.js
+++ api/models/Favorites.js
@@ -5,24 +5,45 @@
  * @description :: A short summary of how this model works and what it represents.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
+// ============================================================================
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+// Purpose:
+//          Save favorive of each user
+// Class:
+//          Favorites Model
+// Functions:
+//          None
+// Called From:
+//          (script) any Controller and sails.config.sockets
+// Author:
+//          Nhien Phan (nhienpv@designveloper.com)
+// Notes:
+//          
+// Changelog:
+//          05/27/2014 - Nhien Phan - Init first revision.
+// ============================================================================
+//
 
+// Now this model not in use
 module.exports = {
-  
-	autoUpdatedAt: false,
-
-	attributes: {
-		
-		id:{
-			type: "integer",
-			primaryKey: true
-		},
-		toUserId:{
-			type: "integer"
-		},
-		fromUserId:{
-			type: "integer"
-		}
-
-	}
+    autoUpdatedAt: false,
+    attributes: {
+        id: {
+            type: "integer",
+            primaryKey: true
+        },
+        toUserId: {
+            type: "integer"
+        },
+        fromUserId: {
+            type: "integer"
+        }
+    }
 
 };
