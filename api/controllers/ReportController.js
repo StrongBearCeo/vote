0a4f99e7dc456b161dacaa977dba8770c52c5603
phanvannhien <phanvannhien@gmail.com>
2014-05-30 17:37:37 +0700

    Create header template file

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -4,7 +4,29 @@
  * @description :: Server-side logic for managing reports
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+// Purpose:
+//          Save report each of user
+// Class:
+//          Report Controller
+// Functions:
+//
+// Called From:
+//          (script) : all controller and sails.config.socket
+// Author:
+//          Nhien Phan(nhienpv@designveloper.com)
+// Notes:
+//
+// Changelog:
+//          05/27/2014 - Nhien Phan - Init first revision.
+// ============================================================================
+//
 module.exports = {
 	
 };
