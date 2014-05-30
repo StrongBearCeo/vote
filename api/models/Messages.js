cc857a1ca5df407964ec6b8e99c7fb8588b1c1ea
phanvannhien <phanvannhien@gmail.com>
2014-05-30 15:38:44 +0700

    Comments code and refactor Messages Model

--- api/models/Messages.js
+++ api/models/Messages.js
@@ -5,13 +5,35 @@
  * @description :: A short summary of how this model works and what it represents.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
+// ============================================================================
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+// Purpose:
+//          Save message chat of each user
+// Class:
+//          Message Model
+// Functions:
+//          None
+// Called From:
+//          (script) any Controller and sails.config.sockets
+// Author:
+//          Nhien Phan (nhienpv@designveloper.com)
+// Notes:
+//
+// Changelog:
+//          05/27/2014 - Nhien Phan - Init first revision.
+// ============================================================================
+//
 
 module.exports = {
-
   	autoUpdatedAt: false,
-
+    // attributes of each fiels model
 	attributes: {
-		
 		id:{
 			type: "integer",
 			primaryKey: true
