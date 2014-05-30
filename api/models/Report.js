7b0724952f14c62f7bf685a1af61ad78c65d0627
phanvannhien <phanvannhien@gmail.com>
2014-05-30 16:35:49 +0700

    Create attributes for Report Model

--- api/models/Report.js
+++ api/models/Report.js
@@ -4,11 +4,48 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+// ============================================================================
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+// Purpose:
+//          Save all report each other user
+// Class:
+//          Report Model
+// Functions:
+//          None
+// Called From:
+//          (script) any Controller and sails.config.sockets
+// Author:
+//          Nhien Phan (nhienpv@designveloper.com)
+// Notes:
+//
+// Changelog:
+//          05/30/2014 - Nhien Phan - Init first revision.
+// ============================================================================
+//
 
 module.exports = {
 
   attributes: {
-
+      id:{
+          type: "integer",
+          primaryKey: true
+      },
+      fromUserId:{
+          type: "integer"
+      },
+      toUserId:{
+          type: "integer"
+      },
+      value: {
+          type: "integer",
+          in: [0,1]
+      }
   }
 };
 
