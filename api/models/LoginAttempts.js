1c9c64f93dcf895ef69338628ebb66e88ee605b7
phanvannhien <phanvannhien@gmail.com>
2014-05-30 15:39:29 +0700

    Coments code and refactor LoginAttempts Model

--- api/models/LoginAttempts.js
+++ api/models/LoginAttempts.js
@@ -5,13 +5,34 @@
  * @description :: A short summary of how this model works and what it represents.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
-
+// ============================================================================
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+// Purpose:
+//          Save loginatemt of each user
+// Class:
+//          LoginAtempts Model
+// Functions:
+//          None
+// Called From:
+//          (script) any Controller and sails.config.sockets
+// Author:
+//          Nhien Phan (nhienpv@designveloper.com)
+// Notes:
+//
+// Changelog:
+//          05/27/2014 - Nhien Phan - Init first revision.
+// ============================================================================
+//
 module.exports = {
-
 	autoUpdatedAt: false,
-
+    // attributes of model
 	attributes: {
-		
 		id:{
 			type: "integer",
 			primaryKey: true
@@ -27,5 +48,4 @@ module.exports = {
 			in: ["success", "fail"]
 		}
 	}
-
 };
