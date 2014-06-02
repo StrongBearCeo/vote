6e5865cd43ae8282077f17a199057bd8eac4bd52
phanvannhien <phanvannhien@gmail.com>
2014-06-02 09:16:58 +0700

    Finish comments code and refacotor file ChatUses Model

--- api/models/ChatUsers.js
+++ api/models/ChatUsers.js
@@ -1,10 +1,3 @@
-/**
- * ChatUsers
- *
- * @module      :: Model
- * @description :: A short summary of how this model works and what it represents.
- * @docs		:: http://sailsjs.org/#!documentation/models
- */
 // ,,,,,,,,, ,,,
 // ,,,,,,,, ,,,  Copyright:
 // ,,,     ,,,          This source is subject to the Designveloper JSC
@@ -23,90 +16,86 @@
 // Author:
 //          Nhien Phan(nhienpv@designveloper.com)
 // Notes:
-//          
+//
 // Changelog:
 //          05/27/2014 - Nhien Phan - Init first revision.
-// ============================================================================
-//
+// =============================================================================
+
 module.exports = {
-		// All properties fields of model
-    autoPK: false,
-    adapter: "memory",
-    attributes: {
+  // All properties fields of model
+  autoPK: false,
+  adapter: "memory",
+  attributes: {
 
-        id: {
-            type: "integer",
-            primaryKey: true
-        },
-        username: {
-            type: "string"
-        },
-        rating: {
-            type: "integer"
-        },
-        favorites: {
-            type: "integer"
-        },
-        time: {
-            type: "integer", // seconds
-            defaultsTo: 30 //30 s for speaker
-        },
-        status: {
-            type: "string",
-            in : ["viewing", "queuing", "speaking", "participant"],
-            /*
-             * 1 speaking, 4 queuing, 2 viewing, and any login is participant
-             *
-             */
-            defaultsTo: "participant"
-        },
-        // load rating / vote count of user
-        loadRating: function(cb) {
-            var self = this;
-            Votes.find({
-                toUserId: self.id
-            }).done(function(error, votes) {
-                if (error) {
-                    cb(error);
-                } else {
-                    self.rating = _.reduce(votes, function(r, v) {
-                        return r + v.value;
-                    }, 0);
+    id: {
+      type: "integer",
+      primaryKey: true
+    },
+    username: {
+      type: "string"
+    },
+    rating: {
+      type: "integer"
+    },
+    favorites: {
+      type: "integer"
+    },
+    time: {
+      type: "integer", // seconds
+      defaultsTo: 30 //30 s for speaker
+    },
+    status: {
+      type: "string",
+      in: ["viewing", "queuing", "speaking", "participant"],
+      /*
+       * 1 speaking, 4 queuing, 2 viewing, and any login is participant
+       *
+       */
+      defaultsTo: "participant"
+    },
+    // load rating / vote count of user
+    loadRating: function (cb) {
+      var self = this;
+      Votes.find({
+        toUserId: self.id
+      }).done(function (error, votes) {
+        if (error) {
+          cb(error);
+        } else {
+          self.rating = _.reduce(votes, function (r, v) {
+            return r + v.value;
+          }, 0);
 
-                    self.save(function(error) {
-                        if (error) {
-                            cb(error);
-                        } else {
-                            cb();
-                        }
-                    });
-                }
-
-            })
-        },
-        // not in use this function
-        loadFavorites: function(cb) {
-            var self = this;
-            Favorites.find({
-                toUserId: self.id
-            }).done(function(error, favorites) {
-                if (error) {
-                    cb(error);
-                } else {
-                    self.favorites = favorites.length;
-                    self.save(function(error) {
-                        if (error) {
-                            cb(error);
-                        } else {
-                            cb();
-                        }
-                    });
-
-                }
-
-            })
+          self.save(function (error) {
+            if (error) {
+              cb(error);
+            } else {
+              cb();
+            }
+          });
         }
 
+      })
+    },
+    // not in use this function
+    loadFavorites: function (cb) {
+      var self = this;
+      Favorites.find({
+        toUserId: self.id
+      }).done(function (error, favorites) {
+        if (error) {
+          cb(error);
+        } else {
+          self.favorites = favorites.length;
+          self.save(function (error) {
+            if (error) {
+              cb(error);
+            } else {
+              cb();
+            }
+          });
+        }
+      })
     }
-
+  }
 };
